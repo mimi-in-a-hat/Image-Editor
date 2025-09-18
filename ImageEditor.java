46ce78ee82268dc6f9f022e07c383db78271bbf6
Emily Zhu <emilyzcat22@gmail.com>
2025-09-18 15:55:31 -0400

    Update ImageEditor.java

--- ImageEditor.java
+++ ImageEditor.java
@@ -1,48 +1,72 @@
 package uk.ac.nulondon;
 
-
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
+import javax.imageio.ImageIO;
 
-/*APPLICATION SERVICE LAYER*/
 public class ImageEditor {
+  private Image image;
+  private final Deque<List<Pixel>> undoSeams = new ArrayDeque();
+  private List<Pixel> highlightedSeam = null;
 
-    private Image image;
+  public ImageEditor() {
+  }
 
-    private List<Pixel> highlightedSeam = null;
+  public void load(String filePath) throws IOException {
+    File originalFile = new File(filePath);
+    BufferedImage img = ImageIO.read(originalFile);
+    this.image = new Image(img);
+  }
 
-    public void load(String filePath) throws IOException {
-        File originalFile = new File(filePath);
-        BufferedImage img = ImageIO.read(originalFile);
-        image = new Image(img);
+  public void save(String filePath) throws IOException {
+    BufferedImage img = this.image.toBufferedImage();
+    ImageIO.write(img, "png", new File(filePath));
+  }
+
+  public void highlightGreenest() throws IOException {
+    this.highlightedSeam = this.image.getGreenestSeam();
+    this.undoSeams.push(this.deepCopySeam(this.highlightedSeam));
+    this.image.higlightSeam(this.highlightedSeam, Color.BLUE);
+  }
+
+  public void highlightLowestEnergySeam() throws IOException {
+    this.highlightedSeam = this.image.getLowestEnergySeam();
+    this.undoSeams.push(this.deepCopySeam(this.highlightedSeam));
+    this.image.higlightSeam(this.highlightedSeam, Color.RED);
+  }
+
+  public void removeHighlighted() throws IOException {
+    if (this.highlightedSeam != null) {
+      this.image.removeSeam(this.highlightedSeam);
+      this.highlightedSeam = null;
     }
 
-    public void save(String filePath) throws IOException {
-        BufferedImage img = image.toBufferedImage();
-        ImageIO.write(img, "png", new File(filePath));
+  }
+
+  public void undo() throws IOException {
+    if (!this.undoSeams.isEmpty()) {
+      List<Pixel> seam = (List)this.undoSeams.pop();
+      this.image.addSeam(seam);
+      this.highlightedSeam = null;
+    }
+  }
+
+  private List<Pixel> deepCopySeam(List<Pixel> seam) {
+    List<Pixel> copy = new ArrayList(seam.size());
+
+    for(Pixel p : seam) {
+      Pixel newPixel = new Pixel(p.color);
+      newPixel.left = p.left;
+      newPixel.right = p.right;
+      copy.add(newPixel);
     }
 
-    public void highlightGreenest() throws IOException {
-        //TODO: implement via Command pattern
-    }
-
-    public void removeHighlighted() throws IOException {
-        //TODO: implement via Command pattern
-    }
-
-    public void undo() throws IOException {
-        //TODO: implement via Command pattern
-    }
-
-    public void highlightLowestEnergySeam() throws IOException {
-        //TODO: implement via Command pattern
-    }
-
-    //TODO: implement Command class or interface and its subtypes
+    return copy;
+  }
 }
